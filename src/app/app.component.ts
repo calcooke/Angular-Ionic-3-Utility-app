edefc9bec348cbc2ce764ee0c6a090cb3d9c0f0e
ziggyzulu <k00083370@student.lit.ie>
2019-08-16 11:42:59 +0100

    Back buttons working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { App, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AppMinimize } from '@ionic-native/app-minimize';
@@ -11,17 +11,36 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private appMinimize: AppMinimize) {
+  constructor(platform: Platform, public app: App, statusBar: StatusBar, splashScreen: SplashScreen, private appMinimize: AppMinimize) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       // splashScreen.hide();
+
+      platform.registerBackButtonAction(() => {
+
+        let nav = app.getActiveNavs()[0];
+        let activeNav = nav.getActive();
+
+        if (activeNav.name === "HomePage"){
+          this.appMinimize.minimize();
+        } else{
+
+          if(nav.canGoBack()){
+            nav.pop();
+          }
+
+        }
+
+      });
+
+
     });
 
-    platform.registerBackButtonAction(() => {
-      this.appMinimize.minimize();
-   });
+  //   platform.registerBackButtonAction(() => {
+  //     this.appMinimize.minimize();
+  //  });
 
   }
 
