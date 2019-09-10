28085b20ef21993e624d7b6c6ceb1b21ba375c3e
ziggyzulu <k00083370@student.lit.ie>
2019-09-11 00:28:30 +0100

    Filter function works, but not functionality.

--- src/components/report-list/report-list.ts
+++ src/components/report-list/report-list.ts
@@ -16,7 +16,7 @@ import {iReport} from '../../interfaces/report';
 export class ReportListComponent implements OnInit {
 
   previousReports:iReport[] = [];
-  
+  searchTerm: string = '';
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public testerDetails: TesterDetailsProvider) {
@@ -55,6 +55,60 @@ export class ReportListComponent implements OnInit {
     this.previousReports[i].open = !this.previousReports[i].open;
    
   }
+
+  resetSearch(){
+
+    console.log("cleared");
+    this.searchTerm = '';
+
+  }
+
+  setFilteredItems(event){
+
+    console.log(event.target.value);
+
+    let test = this.previousReports.filter((item) => {
+      return ((item.testerNo.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1) || 
+  (item.reportNo.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1) || 
+  (item.stickerNo.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1) || 
+  (item.farmID.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1) || 
+  (item.county.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1) || 
+  (item.note.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1) || 
+  (item.testDate.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1));
+    })
+
+    console.log(test);
+    //this.previousReports = test;
+    // let marvelHeroes =  this.previousReports.filter(function(hero) {
+    //   return this.previousReports == event.target.value;
+
+    // const val = event.target.value;
+    // if (val && val.trim() != '') {
+    //   this.previousReports = this.previousReports.filter((item: iReport) => {
+    //     return (item.reportNo.toLowerCase().indexOf(val.toLowerCase()) > -1);
+    //   })
+    // }
+
+    // this.items = this.previousReports.filterItems(this.searchTerm);
+    // this.items.sort();
+     // this.previousReports.filter = this.searchTerm.trim().toLowerCase();
+    //  return this.previousReports.filter((item) => {
+    //   return item.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+    // });
+    // let items = this.filterItems(this.searchTerm);
+    // items.sort();
+  
+  }
+
+  // filterItems(searchTerm){
+
+  //     return this.previousReports.filter((item) => {
+  //       return item.indexOf(searchTerm.toLowerCase()) > -1;
+  //     });
+
+  // }
+
+  
 
 
 }
