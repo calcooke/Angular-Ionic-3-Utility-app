e675cfd4d23f963637297f1616e54aab031e442e
ziggyzulu <k00083370@student.lit.ie>
2019-09-18 23:48:52 +0100

    Form has validation alerts

--- src/providers/fire-store/fire-store.ts
+++ src/providers/fire-store/fire-store.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFirestore} from 'angularfire2/firestore';
 import 'rxjs/add/operator/map';
+import * as firebase from 'firebase/app';
 
 
 
@@ -12,13 +13,39 @@ export class FireStoreProvider {
     
   }
 
-  addReport(report){
+  // addReport(report){
 
-    this.fireStoreDb.collection("reports").add(report);
+  //   this.fireStoreDb.collection("reports").add(report);
+    
+    
+  // }
+
+  addReportOnWake(){
     
   }
 
-  addReportOnWake(){
+
+  addReport(report){
+    
+    let docId:string = report.testerNo;
+    
+    this.fireStoreDb.collection("newLayout").doc(report.testerNo). snapshotChanges().subscribe(doc => {
+       
+      if (!doc.payload.exists)
+      {
+
+        this.fireStoreDb.collection("newLayout").doc(docId).set({});
+      }
+      else{
+        this.fireStoreDb.collection("newLayout").doc(report.testerNo).update({
+
+          reports: firebase.firestore.FieldValue.arrayUnion(report)
+          
+    
+        });
+      }
+
+    });
     
   }
 
